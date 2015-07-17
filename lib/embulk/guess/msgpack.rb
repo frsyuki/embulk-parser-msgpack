401953d6618d4df9c108597277ef3d4dc501e777
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-07-17 15:55:03 -0700

    implemented guess plugin

--- lib/embulk/guess/msgpack.rb
+++ lib/embulk/guess/msgpack.rb
@@ -1,61 +1,89 @@
 module Embulk
   module Guess
 
-    # TODO implement guess plugin to make this command work:
-    #      $ embulk guess -g "msgpack" partial-config.yml
-    #
-    #      Depending on the file format the plugin uses, you can use choose
-    #      one of binary guess (GuessPlugin), text guess (TextGuessPlugin),
-    #      or line guess (LineGuessPlugin).
+    class Msgpack < GuessPlugin
+      Plugin.register_guess("msgpack", self)
 
-    #class Msgpack < GuessPlugin
-    #  Plugin.register_guess("msgpack", self)
-    #
-    #  def guess(config, sample_buffer)
-    #    if sample_buffer[0,2] == GZIP_HEADER
-    #      guessed = {}
-    #      guessed["type"] = "msgpack"
-    #      guessed["property1"] = "guessed-value"
-    #      return {"parser" => guessed}
-    #    else
-    #      return {}
-    #    end
-    #  end
-    #end
+      def guess(config, sample_buffer)
+        return {} unless config.fetch("parser", {}).fetch("type", "msgpack") == "msgpack"
 
-    #class Msgpack < TextGuessPlugin
-    #  Plugin.register_guess("msgpack", self)
-    #
-    #  def guess_text(config, sample_text)
-    #    js = JSON.parse(sample_text) rescue nil
-    #    if js && js["mykeyword"] == "keyword"
-    #      guessed = {}
-    #      guessed["type"] = "msgpack"
-    #      guessed["property1"] = "guessed-value"
-    #      return {"parser" => guessed}
-    #    else
-    #      return {}
-    #    end
-    #  end
-    #end
+        parser_config = config["parser"] || {}
 
-    #class Msgpack < LineGuessPlugin
-    #  Plugin.register_guess("msgpack", self)
-    #
-    #  def guess_lines(config, sample_lines)
-    #    all_line_matched = sample_lines.all? do |line|
-    #      line =~ /mypattern/
-    #    end
-    #    if all_line_matched
-    #      guessed = {}
-    #      guessed["type"] = "msgpack"
-    #      guessed["property1"] = "guessed-value"
-    #      return {"parser" => guessed}
-    #    else
-    #      return {}
-    #    end
-    #  end
-    #end
+        classpath = File.expand_path('../../../../classpath', __FILE__)
+        Dir["#{classpath}/*.jar"].each {|jar| require jar }
 
+        file_encoding = parser_config["file_encoding"]
+        row_encoding = parser_config["row_encoding"]
+
+        if !file_encoding || !row_encoding
+          uk = new_unpacker(sample_buffer)
+          begin
+            n = uk.unpackArrayHeader
+            begin
+              n = uk.unpackArrayHeader
+              file_encoding = "array"
+              row_encoding = "array"
+            rescue org.msgpack.core.MessageTypeException
+              file_encoding = "sequence"
+              row_encoding = "array"
+            end
+          rescue org.msgpack.core.MessageTypeException
+            uk = new_unpacker(sample_buffer)  # TODO unpackArrayHeader consumes buffer (unexpectedly)
+            begin
+              n = uk.unpackMapHeader
+              file_encoding = "sequence"
+              row_encoding = "map"
+            rescue org.msgpack.core.MessageTypeException
+              return {}  # not a msgpack
+            end
+          end
+        end
+
+        uk = new_unpacker(sample_buffer)
+
+        case file_encoding
+        when "array"
+          uk.unpackArrayHeader  # skip array header to convert to sequence
+        when "sequence"
+          # do nothing
+        end
+
+        rows = []
+
+        begin
+          while true
+            rows << JSON.parse(uk.unpackValue.toJson)
+          end
+        rescue java.io.EOFException
+        end
+
+        if rows.size <= 3
+          return {}
+        end
+
+        case row_encoding
+        when "map"
+          schema = Embulk::Guess::SchemaGuess.from_hash_records(rows)
+        when "array"
+          column_count = rows.map {|r| r.size }.max
+          column_names = column_count.times.map {|i| "c#{i}" }
+          schema = Embulk::Guess::SchemaGuess.from_array_records(column_names, rows)
+        end
+
+        parser_guessed = {"type" => "msgpack"}
+        parser_guessed["row_encoding"] = row_encoding
+        parser_guessed["file_encoding"] = file_encoding
+        parser_guessed["columns"] = schema
+
+        return {"parser" => parser_guessed}
+
+      #rescue org.msgpack.core.MessagePackException
+      #  return {}
+      end
+
+      def new_unpacker(sample_buffer)
+        org.msgpack.core.MessagePack.newDefaultUnpacker(sample_buffer.to_java_bytes)
+      end
+    end
   end
 end
